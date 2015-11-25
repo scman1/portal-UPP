6b6feb121b56f3d0641edf4e4f947143d9ac7f14
Abraham Nieva <a_nieva@hotmail.com>
2015-11-25 14:02:20 -0600

    Fix table translations on list runs page

--- app/helpers/runs_helper.rb
+++ app/helpers/runs_helper.rb
@@ -5,7 +5,7 @@ module RunsHelper
       workflow = run.workflow
       version = run.workflow_version
       workflow_version = workflow.find_version(version)
-      created_at = "#{time_ago_in_words(run.created_at)} #{t(".ago")}"
+      created_at = "#{t(".t_ago", how_long: time_ago_in_words(run.created_at))}"
 
       if run.is_a?(Sweep) # If a sweep, get the latest finish time of its runs
         if run.runs.any? {|r| r.finish_time.blank? }
@@ -14,7 +14,7 @@ module RunsHelper
         else
           max_time = run.runs.map { |r| r.finish_time }.max
           finish_time = max_time.to_i
-          finish_time_text = "#{time_ago_in_words(max_time)} #{t(".ago")}"
+          finish_time_text = "#{t(".t_ago", how_long: time_ago_in_words(max_time))}"
         end
       else
         if run.finish_time.blank?
@@ -22,10 +22,11 @@ module RunsHelper
           finish_time_text = "-"
         else
           finish_time = run.finish_time.to_i
-          finish_time_text = "#{time_ago_in_words(run.finish_time)} #{t(".ago")}"
+          finish_time_text = "#{t(".t_ago", how_long: time_ago_in_words(run.finish_time))}"
         end
       end
-
+      #FIXME ugly fix because state tokens are used by the player as keywords
+      run_state = ".rs_#{run.state}"
       [link_to("#{image(run.is_a?(Sweep) ? 'sweep_run' : 'simple_run')}#{run.name}".html_safe,
                run.is_a?(Sweep) ? main_app.sweep_path(run) : taverna_player.run_path(run),
                :class => 'with_icon'
@@ -33,7 +34,7 @@ module RunsHelper
        run.contributor.display_name,
        workflow.can_view? ? link_to(workflow_version.title, main_app.workflow_path(workflow, :version => version)) : workflow_version.title,
        workflow.category.name,
-       "#{run.state.capitalize} #{(run.complete? ? '' : image_tag('ajax-loader.gif', :style => "vertical-align: middle"))}".html_safe,
+       "#{t(run_state)} #{(run.complete? ? '' : image_tag('ajax-loader.gif', :style => "vertical-align: middle"))}".html_safe,
        created_at,
        run.created_at.to_i,
        finish_time_text,
